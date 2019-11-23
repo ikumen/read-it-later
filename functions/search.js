6e9e0ad669855ee3ce519c970599335c257b3bd2
ikumen <ikumen@gnoht.com>
2019-11-22 23:59:22 -0800

    clean up search, added paging

--- functions/search.js
+++ functions/search.js
@@ -1,64 +1,75 @@
 const firebase = require('firebase-admin');
 
-const getPages = (ids) => {
-  const db = firebase.firestore();
-  return Promise.all(ids.map(id => {
-    return db.collection('pages').doc(id).get()
-      .then(doc => ({id, ...doc.data()}));
-  }))
+const DEFAULT_RESULT_SIZE = 10;
+
+/**
+ * Maps page documents from a query to plain old object. 
+ * 
+ * @param {DocumentSnapshot} doc  
+ */
+const pageDocQueryResultMapper = (doc) => {
+  const {title, url, description, createdAt} = doc.data();
+  return {id: doc.id, title, url, description, createdAt};
 }
 
-const listPages = (startAt) => {
+/**
+ * Returns all the page documents referenced by given page ids.
+ * 
+ * @param {String} ids 
+ */
+const getPages = (ids) => {
   const db = firebase.firestore();
-  const pagesCol = db.collection('pages');
+  return Promise.all(ids.map(id => (
+    db.collection('pages').doc(id).get()
+      .then(pageDocQueryResultMapper)
+  )));
+}
 
-  let results;
-  if (startAt) {
-    const startDoc = pagesCol.doc(startAt);
-    results = startDoc.get().then(snapshot => {
-      return pagesCol
-        .orderBy('createdAt', 'desc')
-        .limit(4)
-        .get();
-    });
-  } else {
-    results = pagesCol
-      .orderBy('createdAt', 'desc')
-      .limit(4)
-      .get();
-  }
+/**
+ * Returns list of pages.
+ * 
+ * @param {String} startAt cursor id of document to start query at
+ * @param {Number} limit the result set size
+ */
+const listPages = (startAt, limit) => {
+  const db = firebase.firestore();
+
+  const pagesCol = db.collection('pages');
+  const query = pagesCol
+    .orderBy('createdAt', 'desc')
+    .limit(limit)
+
+  const results = startAt 
+    ? pagesCol.doc(startAt).get()
+        .then(snapshot => query.startAt(snapshot).get())
+    : query.get();
 
   return results.then(snapshot => {
     const pages = [];
-    snapshot.forEach(doc => pages.push({id: doc.id, ...doc.data()}));
+    snapshot.forEach(doc => pages.push(pageDocQueryResultMapper(doc)));
     return pages;
   })
 }
 
 /**
- * @param {*} term 
- * @param {*} startAt 
+ * Find page ids that are associated with the given term.
+ * 
+ * @param {String} term to find 
+ * @param {String} startAt cursor id of document to start query at
+ * @param {Number} limit the result set size
  */
-const findTermPages = async (term, startAt) => {
+const findTermPageIds = async (term, startAt, limit) => {
   const db = firebase.firestore();
-  const termPagesCol = db.collection('terms').doc(term).collection('pages');
   
-  let results;
-  if (startAt) {
-    const startDoc = termPagesCol.doc(startAt);
-    results = startDoc.get().then(snapshot => {
-      return termPagesCol
-        .orderBy('count', 'desc')
-        .startAt(snapshot)
-        .limit(4)
-        .get();
-    });
-  } else {
-    results = termPagesCol
-      .orderBy('count', 'desc')
-      .limit(4)
-      .get();
-  }
+  const termPagesCol = db.collection('terms').doc(term).collection('pages');
+  const query = termPagesCol
+    .orderBy('count', 'desc')
+    .limit(limit);
+
+  const results = startAt 
+    ? termPagesCol.doc(startAt).get()
+        .then(snapshot => query.startAt(snapshot).get())
+    : query.get();
 
   return results.then(snapshot => {
     const pageIds = [];
@@ -67,18 +78,26 @@ const findTermPages = async (term, startAt) => {
   });
 }
 
-const search = (term, startAt) => {
-  return findTermPages(term, startAt).then(getPages);
+/**
+ * Performs search for page documents associated with given term.
+ * 
+ * @param {String} term to find
+ * @param {String} startAt cursor id of document to start search at 
+ * @param {Number} limit the result set size
+ */
+const search = (term, startAt, limit) => {
+  return findTermPageIds(term, startAt, limit).then(getPages);
 }
 
 exports.handler = async (req, res) => {
   const term = (req.query.term || '').trim().toLowerCase();
   const startAt = req.query.at;
+  const limit = parseInt(req.query.limit) || DEFAULT_RESULT_SIZE;
 
   try {
     const pages = term 
-      ? await listPages(startAt) 
-      : await search(term, startAt);
+      ? await search(term, startAt, limit) 
+      : await listPages(startAt, limit);
     return res.status(200).send(pages);
   } catch(error) {
     console.error(`Caught error while searching: ${term}`, error);
