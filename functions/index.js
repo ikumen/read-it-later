cc41ddb092ad9704cf208cb9f35b407c6369611a
ikumen <ikumen@gnoht.com>
2019-11-24 06:26:13 -0800

    Handle CORS, clean up

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,45 @@
 const functions = require('firebase-functions');
 const firebase = require('firebase-admin');
-const fetcher = require('./fetcher');
-const projectId = 'read-it-later-demo';
 
+// fetcher, indexer, search are dependent on Firebase being initialize
 if (firebase.apps.length == 0) {
-  firebase.initializeApp({projectId});
+  firebase.initializeApp();
 }
 
-exports.fetcher = functions.firestore
-  .document('pages/{pageId}')
-  .onCreate(fetcher.handler);
+const fetcher = require('./fetcher');
+const indexer = require('./indexer');
+const search = require('./search');
+
+/**
+ * 
+ * @param {Object} req 
+ * @param {Object} res 
+ * @param {Function} handler 
+ */
+const corsFilter = async (req, res, handler) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') {
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');    
+  } else {
+    await handler(req, res);
+  }
+}
+
+/**
+ * Firestore triggered Function that takes newly created web page document
+ * and fetches the full web page, extracts it's text to Firestore and generates
+ * a PDF to Firebase Storage.
+ */
+exports.fetcher = functions.firestore.document('pages/{pageId}').onCreate(fetcher.handler);
+
+/**
+ * Firestore triggered Function that takes an updated web page document and 
+ * builds an inverted index of the web page's text. The inverted index is saved
+ * to Firestore.
+ */
+exports.indexer = functions.firestore.document('pages/{pageId}').onUpdate(indexer.handler);
+
+exports.search = functions.https.onRequest((req, res) => corsFilter(req, res, search.handler));
--- functions/indexer.js
+++ functions/indexer.js
@@ -1,8 +1,12 @@
-const functions = require('firebase-functions');
 const firebase = require('firebase-admin');
 
 const {STOP_WORDS} = require('./stopwords');
 
+/**
+ * A very simple tokenizer, breaking the given text into terms (i.e, words).
+ * @param {String} text 
+ * @returns hash of terms and count of their occurrences
+ */
 const tokenize = (text) => {
   return (text || '')
     .replace(/[\W_]+/g, ' ') // replace all non alphabetic with space
@@ -13,11 +17,14 @@ const tokenize = (text) => {
     .reduce((terms, token) => { // build a count of each term
       if (!terms[token]) 
         terms[token] = 0;
-      terms[token]++;
+      terms[token]++; // update the count
       return terms;
     }, {});
 }
 
+/**
+ * @param {Object} results 
+ */
 const logBatchResults = (results) => console.log('Batched:', results);
 
 exports.handler = async (change, context) => {
@@ -31,19 +38,22 @@ exports.handler = async (change, context) => {
 
   // Tokenize both the text and title
   const terms = tokenize([text, title].join(' '));
-
-  let i=1;
+  
+  let i=0;
   for (const term in terms) {
-    const ref = db.collection('terms').doc();
+    const ref = db.collection('terms').doc(term).collection('pages').doc(id);
     batch.set(ref, {
-      term: term,
-      page: id,
       count: terms[term]
     });
-
-    if (i++ % 500 == 0 || i >= terms.length) {
+    
+    if (++i % 500 == 0) {
       batch.commit().then(logBatchResults);
       batch = db.batch();
     } 
   }
+
+  if (i % 500 > 0) {
+    batch.commit().then(logBatchResults);
+  }
+
 }
