60e4af674577ece9bb5d87ac987569a1943747aa
ikumen <ikumen@gnoht.com>
2019-11-24 06:24:35 -0800

    Extract and save page description, clean up

--- functions/fetcher.js
+++ functions/fetcher.js
@@ -1,7 +1,5 @@
 const puppeteer = require('puppeteer');
-
-const firestoreService = require('./firestore-service');
-const storageService = require('./storage-service');
+const firebase = require('firebase-admin');
 
 /**
  * Helper for determining if the child requests should be aborted. We abort
@@ -30,6 +28,101 @@ const abortMediaInterceptedRequestHandler = (interceptedRequest) => {
 }
 
 /**
+ * Returns roughly the first ~155 chars of the given text as a description.
+ * @param {String} text 
+ */
+const makeDescription = (text) => {
+  let charCount = 0;
+  return (text || '').substr(0, 300)
+    .split(' ')
+    .filter(word => {
+      if (charCount < 155) {
+        charCount += word.length;
+        return true;
+      }
+      return false;
+    })
+    .join(' ');
+}
+
+/**
+ * Builds an array of meta objects and their attributes. Must use in the 
+ * context of a Puppeteer evaluate() function.
+ * @returns array of meta objects [{meta: {attrs,..}}]
+ */
+const extractMeta = () => {
+  return [...document.querySelectorAll('meta')]
+  .map(elem => {
+    const attr = {};
+    elem.getAttributeNames().forEach(n => attr[n] = elem.getAttribute(n));
+    return attr;
+  });
+}
+
+/**
+ * Look for one of (twitter:|og:)*description meta tags and
+ * use this as the web pages description.
+ * 
+ * @param {Array} metas an array of meta objects and their attributes 
+ */
+const getMetaDescription = (metas) => {
+  let description;
+  metas.forEach(meta => {
+    for(const n in meta) {
+      if ((n === 'name' && (meta[n] === 'description' || meta[n] === 'twitter:description')) 
+          || (n === 'property' && meta[n] === 'og:description')) {
+        description = meta['content'];
+        break;
+      }
+    }
+  });
+  return description;
+}
+
+/**
+ * Save the given page to Firestore.
+ * 
+ * @param {Object} page
+ * @param {String} page.id
+ * @param {String} page.title
+ * @param {String} page.text
+ */
+const savePageToFirestore = async ({id, title, description, text}) => {
+  const db = firebase.firestore();
+  return db.collection('pages').doc(id)
+    .update({
+      title, 
+      description,
+      text
+    });
+}
+
+/**
+ * Saves the given web page PDF contents to Firebase Storage.
+ * 
+ * @param {Object} page
+ * @param {String} page.id Firestore id of web page, will be used as storage filename "id.pdf"
+ * @param {String} page.url URL of web page, will save to storage as metadata
+ * @param {Buffer} contents of web page as PDF buffer
+ */
+savePagePdfToStorage = async ({id, url}, contents) => {
+  return new Promise((resolve, reject) => {
+    const bucket = firebase.storage().bucket();
+    const fileName = `${id}.pdf`;
+    const file = bucket.file(fileName);
+    file.createWriteStream({
+      resumable: false,
+      contentType: 'application/pdf',
+      metadata: {url}
+    })
+    .on('error', reject)
+    .on('finish', () => resolve(fileName))
+    .end(contents);
+  });
+}
+
+
+/**
  * Handles fetching a web page, then extracting the text and generating a PDF, 
  * and finally saving to Firestore and Firebase Storage respectively.
  *  
@@ -43,23 +136,25 @@ exports.handler = async (snapshot, context) => {
   let browser;
 
   try {
-    /* Launch Chrome, in headless mode */
+    // Launch Chrome, in headless mode
     browser = await puppeteer.launch({headless: true, args:['--no-sandbox']});
     const browserPage = await browser.newPage();
 
-    /* Add a request interceptor and ignore any "media" type resources */
+    // Add a request interceptor and ignore any "media" type resources
     await browserPage.setRequestInterception(true);
     browserPage.on('request', abortMediaInterceptedRequestHandler);
 
-    /* Fetch the web page */
+    // Fetch the web page
     await browserPage.goto(url);
 
-    /* Extract web page text and save to Firestore */
+    // Extract web page text and save to Firestore
     const title = await browserPage.title();
     const text = await browserPage.$eval('*', el => el.innerText);
-    await firestoreService.save({id, title, text});
+    const metas = await browserPage.evaluate(extractMeta);
+    const description = getMetaDescription(metas) || makeDescription(text);
+    await savePageToFirestore({id, title, description, text});
 
-    /* Generate PDF version of web page and save to FB Storage */
+    // Generate PDF version of web page and save to FB Storage
     const pdf = await browserPage.pdf();
     await storageService.save({id, url}, pdf);
   
